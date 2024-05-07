ef9b1d403ad095268e3cc680f5a6ea6fc514119f
Jambhale Rajkumar Shivajirao <102852743+RajPatil992@users.noreply.github.com>
2024-05-07 11:34:26 +0530

    Add files via upload

---
+++ CommentInJava.java
@@ -0,0 +1,32 @@
+package com.basic.java;
+
+public class CommentInJava 
+{
+	public static void main(String[]args)
+	{
+
+	// int  a=10; Single line comment
+	
+		
+		
+   /*int a=10; multiline comment
+	int b=20;
+	int c=a+b;
+	System.out.println("The Addition of two number is:"+c);
+	*/
+		
+		
+		/**
+    int a=10; documentation comment
+	int b=20;
+	int c=a+b;
+	System.out.println("The Addition of two number is:"+c);
+		 */
+		
+		
+		
+		
+		
+		
+	}
+}
